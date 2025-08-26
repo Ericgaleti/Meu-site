d50082f8b8f37a6c910694c13a93adc18acf335c
Ericgaleti <151772241+Ericgaleti@users.noreply.github.com>
2025-08-26 12:10:56 -0300

    Add files via upload
    me ajude a como centralizar a div kkkkkkk

---
+++ aa.js
@@ -0,0 +1,13 @@
+var IdFuncionario = 848980;
+
+const entradaDados = require('readline-sync');
+
+console.log('Ola! Qual e o codigo de acesso! \n\n')
+
+let IdFuncionario = entradaDados.question ("digite o codigo de acesso: ");
+
+console.log ("\n\nOla" + IdFuncionario + "\n\n"); + "Seu acesso foi altorizado" && "Seu acesso foi negado";
+
+function alertaVerificacao() {
+    alert ("Seu cadastro esta cendo verificado " + IdFuncionario);
+}
